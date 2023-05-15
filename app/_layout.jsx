58074eb335c009b1033d9afb079d246e98733762
rifat-repliq <rifat.shikder@repliq.dev>
2023-05-15 14:22:28 +0600

    added tanstack query

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,16 +2,25 @@ import { Stack } from "expo-router";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import HeaderCartIcon from "./components/common/HeaderCartIcon";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function Layout() {
   return (
-    <Provider store={store}>
-      <Stack
-        screenOptions={{
-          headerShown: true,
-          headerRight: () => <HeaderCartIcon />,
-        }}
-      />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Stack
+          screenOptions={{
+            headerShown: true,
+            headerRight: () => <HeaderCartIcon />,
+          }}
+        />
+      </Provider>
+    </QueryClientProvider>
   );
 }
--- app/components/ProductList.jsx
+++ app/components/ProductList.jsx
@@ -1,9 +1,17 @@
-import { View, Text, ActivityIndicator, FlatList, Image } from "react-native";
-import useFetch from "../../hooks/useFetch";
+import { View, Text, ActivityIndicator } from "react-native";
+import axios from "axios";
 import ProductCard from "./ProductCard";
+import { useQuery } from "@tanstack/react-query";
 
 export default function ProductList() {
-  const { isLoading, data, error } = useFetch("https://dummyjson.com/products");
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: () =>
+      axios.get("https://dummyjson.com/products").then((res) => {
+        return res.data;
+      }),
+  });
+
   return (
     <View className="flex-1 ">
       <Text className="text-lg pb-4">All Products</Text>
